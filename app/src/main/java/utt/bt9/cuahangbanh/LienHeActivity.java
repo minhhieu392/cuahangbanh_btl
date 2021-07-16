9120c9517c579ca4b43642df2fd4113e66b81782
minhhieu392 <65659805+minhhieu392@users.noreply.github.com>
2021-07-16 08:23:24 +0700

    first commit

---
+++ app/src/main/java/utt/bt9/cuahangbanh/LienHeActivity.java
@@ -0,0 +1,14 @@
+package utt.bt9.cuahangbanh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LienHeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lien_he);
+    }
+}
---
+++ app/src/main/java/utt/bt9/cuahangbanh/MainActivity.java
@@ -0,0 +1,273 @@
+package utt.bt9.cuahangbanh;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar ;
+    ViewFlipper viewFlipper;
+    NavigationView navigationView;
+    RecyclerView recyclerViewmanhinhchinh;
+    ListView listViewmanhinhchinh;
+    DrawerLayout drawerLayout;
+    ArrayList<Loaisp> mangloaisp;
+    LoaispAdapter loaispAdapter;
+    int id = 0;
+    String tenloaisp = "";
+    String hinhanhloaisp = "";
+    ArrayList<Sanpham> mangsanpham;
+    SanphamAdapter sanphamAdapter;
+    public static ArrayList<GioHang> manggiohang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AX();
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+            ActionBar();
+            ActionViewFlipper();
+            GetDuLieuLoaiSanPham();
+            GetDuLieuSPMoiNhat();
+            CatchOnItemListView();
+        }else {
+            CheckConnection.ShowToast_short(getApplicationContext(),"kiem tra lai ket noi");
+            finish();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menugiohang:
+                Intent intent = new Intent(getApplicationContext(),GioHangActivity.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void CatchOnItemListView() {
+        listViewmanhinhchinh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                switch (i){
+                    case 0:
+                        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+                            Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                            startActivity(intent);
+                        }else {
+                            CheckConnection.ShowToast_short(getApplicationContext(),"kiem tra lai ket noi");
+                        }
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case 1:
+                        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+                            Intent intent = new Intent(MainActivity.this,BanhNgotActivity.class);
+                            intent.putExtra("idloaisanpham",mangloaisp.get(i).getId());
+                            startActivity(intent);
+                        }else {
+                            CheckConnection.ShowToast_short(getApplicationContext(),"kiem tra lai ket noi");
+                        }
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case 2:
+                        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+                            Intent intent = new Intent(MainActivity.this,DoUongActivity.class);
+                            intent.putExtra("idloaisanpham",mangloaisp.get(i).getId());
+                            startActivity(intent);
+                        }else {
+                            CheckConnection.ShowToast_short(getApplicationContext(),"kiem tra lai ket noi");
+                        }
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case 3:
+                        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+                            Intent intent = new Intent(MainActivity.this,LienHeActivity.class);
+                            startActivity(intent);
+                        }else {
+                            CheckConnection.ShowToast_short(getApplicationContext(),"kiem tra lai ket noi");
+                        }
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case 4:
+                        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+                            Intent intent = new Intent(MainActivity.this,InfoActivity.class);
+                            startActivity(intent);
+                        }else {
+                            CheckConnection.ShowToast_short(getApplicationContext(),"kiem tra lai ket noi");
+                        }
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                }
+
+            }
+        });
+    }
+
+    private void GetDuLieuSPMoiNhat() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Server.Duongdansanphammoinhat, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response != null){
+                    int ID = 0;
+                    String Tensanpham= "";
+                    Integer Giasanpham = 0;
+                    String Hinhanhsanpham = "";
+                    String Motasanpham = "";
+                    int IDsanpham = 0;
+                    for (int i=0; i<response.length();i++){
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            ID= jsonObject.getInt("id");
+                            Tensanpham = jsonObject.getString("tensanpham");
+                            Giasanpham = jsonObject.getInt("giasanpham");
+                            Hinhanhsanpham = jsonObject.getString("hinhsanpham");
+                            Motasanpham = jsonObject.getString("motasp");
+                            IDsanpham = jsonObject.getInt("idloai");
+                            mangsanpham.add(new Sanpham(ID,Tensanpham,Giasanpham,Hinhanhsanpham,Motasanpham,IDsanpham));
+                            sanphamAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void GetDuLieuLoaiSanPham() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Server.DuongdanLoaisp, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response != null){
+                    for (int i = 0; i< response.length();i++){
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            id=jsonObject.getInt("id");
+                            tenloaisp = jsonObject.getString("tenloaisanpham");
+                            hinhanhloaisp= jsonObject.getString("hinhloai");
+                            mangloaisp.add(new Loaisp(id,tenloaisp,hinhanhloaisp));
+                            loaispAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mangloaisp.add(3,new Loaisp(0,"Liên hệ","https://w7.pngwing.com/pngs/759/922/png-transparent-telephone-logo-iphone-telephone-call-smartphone-phone-electronics-text-trademark-thumbnail.png"));
+                    mangloaisp.add(4,new Loaisp(0,"Thông tin","https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Infobox_info_icon.svg/768px-Infobox_info_icon.svg.png"));
+                    
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                CheckConnection.ShowToast_short(getApplicationContext(),error.toString());
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://tuvaobep.com/wp-content/uploads/2021/05/Cupcake-topping-lo-vi-song.jpg");
+        mangquangcao.add("https://teashop.vn/wp-content/uploads/2020/05/tra-dao-tuoi-giai-khat-cho-mua-he.jpg");
+        for (int i = 0; i< mangquangcao.size();i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Picasso.get().load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        Animation animation_slide_in= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation animation_slide_out= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_out_right);
+        viewFlipper.setInAnimation(animation_slide_in);
+        viewFlipper.setOutAnimation(animation_slide_out);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void AX() {
+        toolbar = findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewflipper);
+        recyclerViewmanhinhchinh = findViewById(R.id.recyclerview);
+        navigationView = findViewById(R.id.navigationview);
+        listViewmanhinhchinh = findViewById(R.id.listviewmanhinhchinh);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        mangloaisp = new ArrayList<>();
+        mangloaisp.add(0,new Loaisp(0,"TASTYCAKE","https://www.pngkit.com/png/detail/41-413073_5422c3418a632d4241caa626-home-icon-home-icons-for-website-png.png"));
+        loaispAdapter = new LoaispAdapter(mangloaisp,getApplicationContext());
+        listViewmanhinhchinh.setAdapter(loaispAdapter);
+        mangsanpham = new ArrayList<>();
+        sanphamAdapter = new SanphamAdapter(getApplicationContext(),mangsanpham);
+        recyclerViewmanhinhchinh.setHasFixedSize(true);
+        recyclerViewmanhinhchinh.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+        recyclerViewmanhinhchinh.setAdapter(sanphamAdapter);
+        if (manggiohang != null){
+
+        }else {
+            manggiohang = new ArrayList<GioHang>();
+        }
+    }
+}
